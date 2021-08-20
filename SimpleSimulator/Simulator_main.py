85ae7f4099aaf1ec2e71cc8d5e57a137da5a743e
kaif <mohammedkaif2106@gmail.com>
2021-08-20 15:52:58 +0400

    Update Simulator_main.py

--- SimpleSimulator/Simulator_main.py
+++ SimpleSimulator/Simulator_main.py
@@ -19,6 +19,22 @@ flags = [0,0,0,0]
 
 pc = 0
 
+def binaryToDecimal(n):
+    return int(n,2)
+
 for line in temp:
-    print(line)
-    
+    opcode = line[:5]
+    if(opcode == "00000"): #add
+        reg1 = line[7:10]
+        reg2= line[10:13]
+        reg3 = line[13:16]
+        reg[binaryToDecimal(reg1)] = reg[binaryToDecimal(reg2)]+reg[binaryToDecimal(reg3)]
+        print("{:08b}".format(pc), end=" ")
+        for i in reg:
+            print("{:016b}".format(i), end = " ")
+        faltu="0"*12
+        flag_val=faltu
+        for i in flags:
+            flag_val+=str(i)
+        print(flag_val)
+        pc+=1
