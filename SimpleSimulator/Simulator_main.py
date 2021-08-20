df7be2d60155b67b5a77dda0d2c3d034dc665f2c
kaif <mohammedkaif2106@gmail.com>
2021-08-20 15:12:41 +0400

    Update Simulator_main.py

--- SimpleSimulator/Simulator_main.py
+++ SimpleSimulator/Simulator_main.py
@@ -1,11 +1,24 @@
 import sys
-temp_l = []
-temp_s = ""
+
 complete_input = sys.stdin.read()
 
+temp=[]
+temp2 = ""
 for char in complete_input:
     if char=='\n':
-        temp_l.append(temp2)
-        temp_s=""
+        temp.append(temp2)
+        temp2=""
     else:
-        temp_s+=char
+        temp2+=char
+if temp2!="":
+    temp.append(temp2)
+    temp2=""
+
+reg = [0,0,0,0,0,0,0]
+flags = [0,0,0,0]
+
+pc = 0
+
+for line in temp:
+    print(line)
+    
