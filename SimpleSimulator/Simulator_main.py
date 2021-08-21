3ced5d1f31610529645166c55f65707d3acadd55
kaif <mohammedkaif2106@gmail.com>
2021-08-21 17:23:12 +0400

    Update Simulator_main.py

--- SimpleSimulator/Simulator_main.py
+++ SimpleSimulator/Simulator_main.py
@@ -1,7 +1,5 @@
 import sys
-
 complete_input = sys.stdin.read()
-
 temp=[]
 temp2 = ""
 for char in complete_input:
@@ -13,16 +11,26 @@ for char in complete_input:
 if temp2!="":
     temp.append(temp2)
     temp2=""
-
 reg = [0,0,0,0,0,0,0]
 flags = [0,0,0,0]
-
 pc = 0
-
 def binaryToDecimal(n):
     return int(n,2)
-j = 0
 
+
+def fn(pc):
+    
+    print("{:08b}".format(pc), end=" ")
+    for i in reg:
+        print("{:016b}".format(i), end = " ")
+    faltu="0"*12
+    flag_val=faltu
+    for i in flags:
+        flag_val+=str(i)
+    print(flag_val)
+    pc+=1
+
+j = 0
 while j<len(temp):
     line = temp[j]
     opcode = line[:5]
@@ -32,46 +40,94 @@ while j<len(temp):
         reg2= line[10:13]
         reg3 = line[13:16]
         reg[binaryToDecimal(reg1)] = reg[binaryToDecimal(reg2)]+reg[binaryToDecimal(reg3)]
-        print("{:08b}".format(pc), end=" ")
-        for i in reg:
-            print("{:016b}".format(i), end = " ")
-        faltu="0"*12
-        flag_val=faltu
-        for i in flags:
-            flag_val+=str(i)
-        print(flag_val)
+        if(reg[binaryToDecimal(reg1)]>pow(2,16)):
+            flags[0]=1
+            reg[binaryToDecimal(reg1)] = pow(2,16)-reg[binaryToDecimal(reg1)]
+        fn(pc)
         pc+=1
+
+    elif(opcode=="00010"): #mov imm
+        flags=[0,0,0,0]
+        reg1 = binaryToDecimal(line[5:8])
+        imm_val = binaryToDecimal(line[8:16])
+        reg[reg1] = imm_val
+        fn(pc)
+        pc+=1
+    
+    elif(opcode=="00011"): #mov reg
+        flags=[0,0,0,0]
+        reg1 = binaryToDecimal(line[10:13])
+        reg2 = binaryToDecimal(line[13:16])
+        reg[reg1] = reg[reg2]
+        fn(pc)
+        pc+=1
+
+
+
+
+
     elif (opcode == "01111"): #jmp uncd 
         flags = [0,0,0,0]
         j = binaryToDecimal(line[8:])
+        fn(pc)
+        pc+=1
+        j+=1
         continue
+
     elif (opcode == "10000")     :
         if flags[1]==1:
             j = binaryToDecimal(line[8:])
             flags = [0,0,0,0]
+            fn(pc)
+            pc+=1
+            j+=1
             continue
         flags = [0,0,0,0]
+        fn(pc)
+        pc+=1
+
     elif (opcode == "10001")     :
         if flags[2]==1:
             j = binaryToDecimal(line[8:])
             flags = [0,0,0,0]
+            fn(pc)
+            pc+=1
+            j+=1
             continue
-        flags = [0,0,0,0]    
+        flags = [0,0,0,0]
+        fn(pc)
+        pc+=1    
+
     elif (opcode == "10010")     :
         if flags[3]==1:
             j = binaryToDecimal(line[8:])
             flags = [0,0,0,0]
+            fn(pc)
+            pc+=1
+            j+=1
             continue
         flags = [0,0,0,0]
+        fn(pc)
+        pc+=1
+
+    elif (opcode == "01110"): #cmp
+        reg1=line[10:13]
+        reg2 = line[13:16]
+        if(binaryToDecimal(reg1)>binaryToDecimal(reg2)):
+            flags[2]=1
+        elif(binaryToDecimal(reg1)==binaryToDecimal(reg2)):
+            flags[3]=1
+        else:
+            flags[1]=1
+        fn(pc)
+        pc+=1
+    
+    
+
+
     elif(opcode == "10011"): #halt
         flags = [0,0,0,0]
-        print("{:08b}".format(pc), end=" ")
-        for i in reg:
-            print("{:016b}".format(i), end = " ")
-        faltu="0"*12
-        flag_val=faltu
-        for i in flags:
-            flag_val+=str(i)
-        print(flag_val)
-        pc+=1                        
-    j=j+1 
+        fn(pc)
+        pc+=1
+
+    j+=1
