102509ebdaa099c785347f3725ade268875fa797
kaif <mohammedkaif2106@gmail.com>
2021-08-21 15:01:28 +0400

    Update Simulator_main.py

--- SimpleSimulator/Simulator_main.py
+++ SimpleSimulator/Simulator_main.py
@@ -2,6 +2,9 @@ import sys
 
 complete_input = sys.stdin.read()
 
+
+memory={}
+
 temp=[]
 temp2 = ""
 for char in complete_input:
@@ -22,10 +25,17 @@ pc = 0
 def binaryToDecimal(n):
     return int(n,2)
 
-for line in temp:
+memory_counter = 0
+
+j= 0
+end = len(temp)
+print(end)
+
+while(j<end):
+    line = temp[j]
     opcode = line[:5]
     if(opcode == "00000"): #add
-        flags = [0,0,0,0]
+        flags=[0,0,0,0]
         reg1 = line[7:10]
         reg2= line[10:13]
         reg3 = line[13:16]
@@ -39,3 +49,48 @@ for line in temp:
             flag_val+=str(i)
         print(flag_val)
         pc+=1
+        
+    if(opcode == "10011"): #halt
+        flags = [0,0,0,0]
+        print("{:08b}".format(pc), end=" ")
+        for i in reg:
+            print("{:016b}".format(i), end = " ")
+        faltu="0"*12
+        flag_val=faltu
+        for i in flags:
+            flag_val+=str(i)
+        print(flag_val)
+        pc+=1
+
+    # if(opcode == "00101"): #store
+    #     flags = [0,0,0,0,0]
+    #     reg1 = line[5:8]
+    #     mem_addr= line[8:16]
+        
+    #     memory[mem_addr]=reg[binaryToDecimal(reg1)]
+    #     print("{:08b}".format(pc), end=" ")
+    #     for i in reg:
+    #         print("{:016b}".format(i), end = " ")
+    #     faltu="0"*12
+    #     flag_val=faltu
+    #     for i in flags:
+    #         flag_val+=str(i)
+    #     print(flag_val)
+    #     pc+=1
+    # if(opcode == "00100"): #load
+    #     flags = [0,0,0,0,0]
+    #     reg1 = line[5:8]
+    #     mem_addr= line[8:16]
+    #     reg[binaryToDecimal(reg1)] = memory[mem_addr]
+    #     print("{:08b}".format(pc), end=" ")
+    #     for i in reg:
+    #         print("{:016b}".format(i), end = " ")
+    #     faltu="0"*12
+    #     flag_val=faltu
+    #     for i in flags:
+    #         flag_val+=str(i)
+    #     print(flag_val)
+    #     pc+=1
+
+    
+    j+=1
